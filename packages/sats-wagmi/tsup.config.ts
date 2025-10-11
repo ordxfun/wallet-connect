4b2d56de56b38a18c305f43b5dc250ad4fb1e79e
ordxsat <sat@ordx.fun>
2025-10-11 22:39:05 +0800

    chore: fix build configuration and successfully publish package to npm

--- packages/sats-wagmi/tsup.config.ts
+++ packages/sats-wagmi/tsup.config.ts
@@ -4,5 +4,14 @@ export default defineConfig({
   clean: true,
   target: 'es2019',
   format: ['cjs', 'esm'],
-  banner: { js: '"use client";' }
+  banner: { js: '"use client";' },
+  outDir: 'dist',
+  entry: ['src/index.ts'],
+  dts: true,
+  splitting: false,
+  sourcemap: true,
+  minify: false,
+  shims: true,
+  skipNodeModulesBundle: true,
+  external: ['react', 'react-dom']
 });
